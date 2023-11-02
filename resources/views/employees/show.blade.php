8518c0817fb8b96dfb19a412cc22b499fcc36856
Ahmad Sukri(Bettra) <128575451+Bettra@users.noreply.github.com>
2023-11-02 16:01:04 +0700

    first commit

---
+++ resources/views/employees/show.blade.php
@@ -0,0 +1,50 @@
+@extends('employees.layout')
+  
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Emplpoyee</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('employees.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $employee->name }}
+            </div>
+        </div>
+        <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Ktp:</strong>
+                {{ $employee->ktp }}
+            </div>
+        </div>
+        <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Address:</strong>
+                {{ $employee->address }}
+            </div>
+        </div>
+        <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Mobile_phone:</strong>
+                {{ $employee->mobile_phone }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Date_of_birth:</strong>
+                {{ $employee->date_of_birth }}
+            </div>
+        </div>
+    </div>
+@endsection
